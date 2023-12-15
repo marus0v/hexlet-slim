f005e80acafe9d2914045f5d95c5a1b06f5df6dd
Andrey <andrey.marus@gmail.com>
2023-12-15 11:57:46 +0300

    refactored some shit

--- public/index.php
+++ public/index.php
@@ -3,10 +3,33 @@
 // Подключение автозагрузки через composer
 require __DIR__ . '/../vendor/autoload.php';
 
+// Подключение сторонних библиотек
 use Slim\Factory\AppFactory;
 use DI\Container;
 use App\Validator;
 
+// СТАРТ СЕССИИ
+session_start();
+
+// ПОДКЛЮЧЕНИЕ КОНТЕЙНЕРОВ
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+$container->set('flash', function () {
+    return new \Slim\Flash\Messages();
+});
+
+AppFactory::setContainer($container);
+$app = AppFactory::create();
+
+/* $app->get('/users', function ($req, $res) {
+    $this->get('flash')->addMessage('success', 'This is a message');
+    return $res->withRedirect('/users');
+}); */
+
+// СЧИТЫВАНИЕ ДАННЫХ ИЗ JSON
 function getFileData(string $fileName): string
 {
     if (!file_exists($fileName)) {
@@ -22,17 +45,27 @@ function getArrayFromJson(string $fileName)
 }
 
 // $users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
+$repo = getArrayFromJson('./src/repo.json');
 
 $container = new Container();
 $container->set('renderer', function () {
     // Параметром передается базовая директория, в которой будут храниться шаблоны
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 });
+$container->set('flash', function () {
+    return new Slim\Flash\Messages();
+});
+
 $app = AppFactory::createFromContainer($container);
 
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function ($request, $response) {
+$router = $app->getRouteCollector()->getRouteParser();
+
+// ДОМАШНЯЯ СТРАНИЦА
+$app->get('/', function ($request, $response) use ($router) {
+    $router->urlFor('users');
+    $router->urlFor('user/new');
     return $response->write('Welcome to Slim!');
 });
 
@@ -48,32 +81,46 @@ $app->get('/', function ($request, $response) {
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);;
 }); */
 
-$repo = getArrayFromJson('./src/repo.json');
-// $user_id = count($repo);
-// var_dump($repo);
-var_dump(count($repo));
+// ПОСТРАНИЧНЫЙ ВЫВОД СПИСКА ПОЛЬЗОВАТЕЛЕЙ
+$app->get('/users', function ($request, $response) use ($repo) {
+    // $allPosts = $repo->all();
+    $page = $request->getQueryParam('page', 1);
+    $per = $request->getQueryParam('per', 5);
+    $users = array_slice($repo, ($page - 1) * $per, $per);
+    $params = [
+    'users' => $users,
+    'page' => $page
+    ];
+    return $this->get('renderer')->render($response, 'users/index.phtml', $params);
+    //            ->get('flash')->addMessage('success', 'This is a message');
+})->setName('users');
 
-$app->get('/users', function ($request, $response) use ($repo, $user_id) {
+/* $app->get('/users', function ($request, $response) use ($repo, $user_id) {
     $params = [
         'users' => $repo
     ];
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
-});
+})->setName('users'); */
 
+// ДОБАВЛЕНИЕ ПОЛЬЗОВАТЕЛЯ
 $app->post('/users', function ($request, $response) use ($repo) {
     $validator = new Validator();
     $user_id = count($repo);
-    $user['id'] = $user_id;
+    // $user['id'] = $user_id;
     $user = $request->getParsedBodyParam('user');
     $errors = $validator->validate($user);
     if (count($errors) === 0) {
+        $user['id'] = $user_id;
         $repo[] = $user;
         file_put_contents('./src/repo.json', json_encode($repo));
-        return $response->withRedirect('/users', 302);
+        // $this->get('flash')->addMessage('success', 'User has been created');
+        return $response->withRedirect('/users', 302)
+                        ->get('flash')->addMessage('success', 'User has been created');
     }
     $params = [
         'user' => $user,
-        'errors' => $errors
+        'errors' => $errors,
+        'flash' => $flash
     ];
     return $this->get('renderer')
                 ->render($response->withStatus(422), 'users/new.phtml', $params);
@@ -81,20 +128,91 @@ $app->post('/users', function ($request, $response) use ($repo) {
 
 $app->get('/users/new', function ($request, $response) {
     $params = [
-        'user' => ['name' => '', 'email' => ''],
+        'user' => ['name' => '', 'email' => '', 'id' => ''],
         'errors' => []
     ];
     return $this->get('renderer')->render($response, "users/new.phtml", $params);
+})->setName('user/new');
+
+// ИЗМЕНЕНИЕ ПОЛЬЗОВАТЕЛЯ
+$app->get('/users/{id}/edit', function ($request, $response, array $args) use ($repo) {
+    $id = $args['id'];
+    $user = array_filter($repo, fn($user) => $user['id'] == $id)[$id];
+    // $user = $repo->find($id);
+    $params = [
+        'user' => $user,
+        'errors' => []
+    ];
+    return $this->get('renderer')->render($response, 'users/index.phtml', $params);
+})->setName('editUser');
+
+$app->patch('/users/{id}', function ($request, $response, $args) use ($repo, $router) {
+    // $repo = new App\PostRepository();
+    $id = $args['id'];
+    $user = array_filter($repo, fn($user) => $user['id'] == $id)[$id];
+    // $user = $repo->find($id);
+    $newUser = $request->getParsedBodyParam('user');
+    $validator = new Validator();
+    $errors = $validator->validate($data);
+
+    if (count($errors) === 0) {
+        $user['name'] = $newUser['name'];
+        $user['body'] = $newUser['body'];
+        $this->get('flash')->addMessage('success', 'User has been updated');
+        $repo->save($user);
+        $url = $router->urlFor('editUser', ['id' => $user['id']]);
+        return $response->withRedirect($url);
+    }
+
+    $params = [
+        'user' => $user,
+        'errors' => $errors
+    ];
+
+    $response = $response->withStatus(422);
+    return $this->get('renderer')->render($response, 'posts/edit.phtml', $params);
 });
 
 // $app->post('/users', function ($request, $response) {
 //    return $response->withStatus(302);
 // });
 
-$app->get('/courses/{id}', function ($request, $response, array $args) {
+/* $app->get('/users/{id}', function ($request, $response, array $args) use ($repo) {
     $id = $args['id'];
-    return $response->write("Course id: {$id}");
-});
+    $user = array_filter($repo, fn($user) => $user['id'] == $id);
+    // var_dump($user);
+    if (!$user) {
+        return $response->write('Page not found')
+                        ->withStatus(404);
+    }
+    return $response->write("User {$id} is called {$user[$id]['name']}!");
+})->setName('user'); */
+
+$app->get('/posts', function ($request, $response) use ($repo) {
+    $allPosts = $repo->all();
+    $page = $request->getQueryParam('page', 1);
+    $per = $request->getQueryParam('per', 5);
+    $posts = array_slice($allPosts, ($page - 1) * $per, $per);
+    // var_dump($allPosts);
+    $params = [
+    'posts' => $posts,
+    'page' => $page
+    ];
+    return $this->get('renderer')->render($response, 'posts/index.phtml', $params);
+})->setName('posts');
+
+$app->get('/users/{id}', function ($request, $response, array $args) use ($repo) {
+    $id = $args['id'];
+    $user = array_filter($repo, fn($user) => $user['id'] == $id)[$id];
+    if (!$user) {
+        return $response->write('Page not found')
+                        ->withStatus(404);
+    }
+    $params = [
+    'user' => $user
+    ];
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+})->setName('user');
 
 /* $app->get('/users/{id}', function ($request, $response, $args) {
     $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
